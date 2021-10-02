4434abfb77a64b3a0d3fa9acf93cedacc182d44b
cthacker-udel <cthacker@udel.edu>
2021-10-02 04:18:11 -0400

    update pictures

--- src/components/PokerCard.tsx
+++ src/components/PokerCard.tsx
@@ -2,10 +2,12 @@ import Image from 'react-bootstrap/Image';
 
 export function PokerCard(props: {cardName: string}): JSX.Element{
 
+    //console.log(`url = ${process.env.PUBLIC_URL}/cards/${props.cardName}.PNG`);
+
     return(
 
         
-        <Image src={`${process.env.PUBLIC_URL}/assets/cards/${props.cardName}.PNG`} style={{height: "100px", width: "100px"}} rounded />
+        <Image src={`${process.env.PUBLIC_URL}/cards/${props.cardName}.PNG`} style={{height: "100px", width: "100px"}} rounded />
 
 
     );
